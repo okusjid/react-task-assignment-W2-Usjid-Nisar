a8439e0393edbda598e66137da1610e1b870ef3b
okusjid <l200927@lhr.nu.edu.pk>
2024-08-05 14:26:22 +0500

    Updated loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,10 +3,14 @@ import { RotateLoader } from 'react-spinners';
 
 function Loader() {
   return (
-    <div className='loader'>
-      <div className='loader-text'>Loading Movies...</div>
-      <RotateLoader color='#000' loading={true} size={25} />
-    </div>
+    <>
+      {/* <div className='loader-text'>Loading Movies...</div> */}
+      <div className='loader'>
+        <RotateLoader color='#000' loading={true} size={25} />
+      </div>
+      {/* <h2>Please Wait</h2> */}
+
+    </>
   );
 }
 
